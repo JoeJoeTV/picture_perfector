2dc0ea64cb1bfc44d8e104875b7e0fdf1a60a634
Johannes Schöneberger <hannes1033@gmail.com>
2023-11-20 18:21:14 +0100

    Add smoothing of of combine operations

--- src/shapes/sdf/combine.cpp
+++ src/shapes/sdf/combine.cpp
@@ -4,15 +4,28 @@
 namespace lightwave {
 
 enum CombineMode {
-    UNION, SUB, INTERSECT, XOR
+    UNION, SUB, INTERSECT
 };
+
+// See https://iquilezles.org/articles/distfunctions/
+float smoothUnion(const float d1, const float d2, const float k) {
+    float h = max(k - abs(d1-d2), 0.0f);
+    return min(d1, d2) - h*h*0.25/k;
+}
 
 class SDFCombine : public SDFShape {
     /// @brief SDF Shapes combines using this min pseudo shape
     ref<SDFShape> m_leftSDF;
     ref<SDFShape> m_rightSDF;
 
+    /// @brief Describes how the two SDFs should be combined
     CombineMode m_combineMode;
+
+    /// @brief Wether to use smooth union, intersection and subtraction operations
+    bool m_smooth;
+
+    /// @brief The size of the smoothing
+    float m_smooth_size;
 public:
     SDFCombine(const Properties &properties) {
         const std::vector<ref<SDFShape>> sdf_children = properties.getChildren<SDFShape>();
@@ -29,26 +42,39 @@ public:
                     {"union", CombineMode::UNION},
                     {"sub", CombineMode::SUB},
                     {"intersect", CombineMode::INTERSECT},
-                    {"xor", CombineMode::XOR},
                 }
             );
+        
+        this->m_smooth = properties.get<bool>("smooth", false);
+        this->m_smooth_size = properties.get<float>("k", 1.0f);
     }
 
     float estimateDistance(const Point p) const override {
         const float EDLeft = this->m_leftSDF->estimateDistance(p);
         const float EDRight = this->m_rightSDF->estimateDistance(p);
         
-        switch (this->m_combineMode) {
-        case CombineMode::UNION:
-            return min(EDLeft, EDRight);
-        case CombineMode::SUB:
-            return max(-EDLeft, EDRight);
-        case CombineMode::INTERSECT:
-            return max(EDLeft, EDRight);
-        case CombineMode::XOR:
-            return max(min(EDLeft, EDRight), -min(EDLeft, EDRight));
-        default:
-            return 0;
+        if (this->m_smooth) {
+            switch (this->m_combineMode) {
+            case CombineMode::UNION:
+                return smoothUnion(EDLeft, EDRight, this->m_smooth_size);
+            case CombineMode::SUB:
+                return -smoothUnion(EDLeft, -EDRight, this->m_smooth_size);
+            case CombineMode::INTERSECT:
+                return -smoothUnion(-EDLeft, -EDRight, this->m_smooth_size);
+            default:
+                return 0;
+            }
+        } else {
+            switch (this->m_combineMode) {
+            case CombineMode::UNION:
+                return min(EDLeft, EDRight);
+            case CombineMode::SUB:
+                return max(-EDLeft, EDRight);
+            case CombineMode::INTERSECT:
+                return max(EDLeft, EDRight);
+            default:
+                return 0;
+            }
         }
     }
 
