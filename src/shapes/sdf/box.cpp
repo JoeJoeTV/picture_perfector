017bd34a0de67988dccbc696aff43e710a14a509
Johannes Schöneberger <hannes1033@gmail.com>
2023-11-23 13:40:18 +0100

    Rename cube sdf to box and add rounded corners

--- src/shapes/sdf/box.cpp
+++ src/shapes/sdf/box.cpp
@@ -3,19 +3,24 @@
 
 namespace lightwave {
 
-class SDFCube : public SDFShape {
+class SDFBox : public SDFShape {
     /// @brief The corner of the cube specifying the size
     Point m_corner;
+
+    /// @brief The corner radius for the edges of the box. Set to 0 to disable
+    float m_cornerRadius;
 
     /// @brief Transform for SDF
     ref<Transform> m_transform;
 public:
-    SDFCube(const Properties &properties) {
+    SDFBox(const Properties &properties) {
         this->m_corner = Point{
             properties.get<float>("cx", 1.0f),
             properties.get<float>("cy", 1.0f),
             properties.get<float>("cz", 1.0f)
         };
+
+        this->m_cornerRadius = properties.get<float>("radius", 0.0f);
 
         this->m_transform = properties.getOptionalChild<Transform>();
     }
@@ -35,21 +40,23 @@ public:
         Vector q = absP - this->m_corner;
         Vector maxQ0 = Vector{max(q.x(), 0.0f), max(q.y(), 0.0f), max(q.z(), 0.0f)};
         
-        return maxQ0.length() + min(max(q.x(), max(q.y(), q.z())), 0.0f);
+        return maxQ0.length() + min(max(q.x(), max(q.y(), q.z())), 0.0f) - this->m_cornerRadius;
     }
 
     std::string toString() const override {
         return tfm::format(
-            "SDFCube[\n"
+            "SDFBox[\n"
             "  corner = (%f, %f, %f),\n"
+            "  radius = %f,\n"
             "]",
             this->m_corner.x(),
             this->m_corner.y(),
-            this->m_corner.z()
+            this->m_corner.z(),
+            this->m_cornerRadius
         );
     }
 };
     
 }
 
-REGISTER_CLASS(SDFCube, "sdf", "cube")
+REGISTER_CLASS(SDFBox, "sdf", "box")
