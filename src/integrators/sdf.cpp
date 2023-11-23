cbc0258f307e643ba67b6adc6628b72a8b0f9334
Johannes Schöneberger <hannes1033@gmail.com>
2023-11-23 13:41:08 +0100

    Add integrator to show SDFs based upon steps marched

---
+++ src/integrators/sdf.cpp
@@ -0,0 +1,45 @@
+#include <lightwave.hpp>
+
+namespace lightwave {
+class SDFIntegrator : public SamplingIntegrator {
+    /// @brief Whether to remap the normal from [-1,1] to [0,1] 
+    bool m_remap;
+
+public:
+    SDFIntegrator(const Properties &properties)
+    : SamplingIntegrator(properties) {
+        // Do nothing?
+    }
+
+    /**
+     * @brief The job of an integrator is to return a color for a ray produced by the camera model.
+     * This will be run for each pixel of the image, potentially with multiple samples for each pixel.
+     */
+    Color Li(const Ray &ray, Sampler &rng) override {
+        // intersect the ray with the scene
+        Intersection its = m_scene->intersect(ray, rng);
+
+        // if no intersection occured
+        if (!its) {
+            return Color(0.0f);
+        } else {
+            return Color(its.uv[0]);
+        }
+    }
+
+    /// @brief An optional textual representation of this class, which can be useful for debugging. 
+    std::string toString() const override {
+        return tfm::format(
+            "SDFIntegrator[\n"
+            "  sampler = %s,\n"
+            "  image = %s,\n"
+            "]",
+            indent(m_sampler),
+            indent(m_image)
+        );
+    }
+};
+
+}
+
+REGISTER_INTEGRATOR(SDFIntegrator, "sdf")
