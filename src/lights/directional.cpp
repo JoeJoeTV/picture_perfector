83dc5b5929ab98bca476fd4d508072127396535e
Johannes Schöneberger <hannes1033@gmail.com>
2023-12-08 20:09:08 +0100

    Implement point and direct light types

---
+++ src/lights/directional.cpp
@@ -0,0 +1,44 @@
+#include <lightwave.hpp>
+
+namespace lightwave {
+
+class DirectionalLight final : public Light {
+    /// @brief The intensity of the light emitted from the light source
+    Color m_intensity;
+
+    /// @brief The direction in which the light is emitted
+    Vector m_direction;
+public:
+    DirectionalLight(const Properties &properties) {
+        this->m_intensity = properties.get<Color>("intensity");
+        this->m_direction = properties.get<Vector>("direction").normalized();
+    }
+
+    DirectLightSample sampleDirect(const Point &origin,
+                                   Sampler &rng) const override {
+        const Vector wi = -this->m_direction;
+
+        return DirectLightSample{
+            .wi = wi,
+            .weight = m_intensity * Frame::cosTheta(wi),
+            .distance = Infinity
+        };
+
+    }
+
+    bool canBeIntersected() const override { return false; }
+
+    std::string toString() const override {
+        return tfm::format(
+            "DirectionalLight[\n"
+            "  intensity = %s,\n"
+            "  direction = %s,\n"
+            "]",
+            this->m_intensity,
+            this->m_direction);
+    }
+};
+
+} // namespace lightwave
+
+REGISTER_LIGHT(DirectionalLight, "directional")
--- src/lights/point.cpp
+++ src/lights/point.cpp
@@ -3,20 +3,44 @@
 namespace lightwave {
 
 class PointLight final : public Light {
+    /// @brief The power/flux emitted from the point light
+    Color m_power;
+
+    /// @brief The position at which the light source resides
+    Point m_position;
 public:
     PointLight(const Properties &properties) {
+        this->m_power = properties.get<Color>("power");
+        this->m_position = properties.get<Point>("position");
     }
 
     DirectLightSample sampleDirect(const Point &origin,
                                    Sampler &rng) const override {
-        NOT_IMPLEMENTED
+        const Vector wi = (this->m_position - origin).normalized();
+        const float distance = (this->m_position - origin).length();
+
+        // Since the point light source is emitting light in every direction,
+        // we divide the power output by 4π to get the intensity
+        const Color intensity = this->m_power / (4 * Pi);
+
+        return DirectLightSample{
+            .wi = wi,
+            .weight = intensity / sqr(distance),
+            .distance = distance
+        };
+
     }
 
     bool canBeIntersected() const override { return false; }
 
     std::string toString() const override {
-        return tfm::format("PointLight[\n"
-                           "]");
+        return tfm::format(
+            "PointLight[\n"
+            "  power = %s,\n"
+            "  position = %s,\n"
+            "]",
+            this->m_power,
+            this->m_position);
     }
 };
 
